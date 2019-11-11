aeacc624818554d3f50d2e37510f058b286c96e0
Italo Freitas <itallo.freitass@gmail.com>
2019-11-11 13:05:20 -0300

    Quiz UniFanor - Sematec v1.1 (correção nas respostas)

--- questions.js
+++ questions.js
@@ -1,16 +1,16 @@
 var questions = [
     ["como é denominado o mecanismo adequado para realizar o bloqueio de conexões FTP e datagramas UDP?"
-     ,"IPS","VLAN","Firewall de filtragem de pacotes", "Gateway de aplicação.","3"],
-    ["A ameaça de segurança em que o atacante consegue inserir uma série de instruções SQL dentro de uma consulta é conhecida como ?",
-     "SQL False Query", "SQL Fake Query", " SQL Query Attack", "SQL Injection.","4"],
+     ,"IPS","VLAN","Firewall de filtragem de pacotes", "Gateway de aplicação","3"],
+    ["A ameaça de segurança em que o atacante consegue inserir uma série de instruções SQL dentro de uma consulta é conhecida como?",
+     "SQL False Query", "SQL Fake Query", " SQL Query Attack", "SQL Injection","4"],
     [" Um algoritmo de encriptação de dados", "DES", "ID3", "MD5share", ".Quickensort","1"],
     ["Qual o objetivo original de particionar um disco com mais de um disco lógico:", "Ganhar poder de processamento", " Segmentar os programas com arquivos de trabalho", "Segmentar programas", "Criar controles de acesso para usuários","2"],
     [" São exemplos, respectivamente, de um Firewall e de um sistema de detecção de intrusão:", "Nmap e Snort", " Kerberos e NMap", " IPTables e Snort", "IPTables e Kerberos","3"],
     [" A técnica sofisticada de autenticar uma máquina como outra forjando pacotes de um endereço de origem confiável, é conhecida como:", "Engenharia Social", "  Negação de serviço", " Engenharia reversa", "Spoofing","4"],
     [" NÃO é um algoritmo de chave simétrica o sistema de criptografia de chave", "única", "pública", "secreta","simétrica", "2"],
     [" Vulnerabilidade é o (a)", "ponto fraco de um ativo que pode ser explorado por uma ameaça", "potencial causa de um incidente não desejado que pode resultar em danos à organização", "processo de identificar, reconhecer e tratar riscos", "efeito da incerteza quanto aos objetivos", "1"],
-    ["Em relação aos conceitos de segurança da informação. são pilares, exceto :", "Disponibilidade", "Integridade", "Confidencialidade", "Simplicidade", "4"],
-    ["A respeito de ataques a redes de computadores. O registro de eventos log ajuda a identificar e a rastrear as ações executadas em um computador", "Vedadeiro", "Falso", "Raramente", "Nenhuma das opções", "1"],   
+    ["Em relação aos conceitos de segurança da informação. são pilares, exceto:", "Disponibilidade", "Integridade", "Confidencialidade", "Simplicidade", "4"],
+    ["A respeito de ataques a redes de computadores. O registro de eventos log ajuda a identificar e a rastrear as ações executadas em um computador.", "Vedadeiro", "Falso", "Raramente", "Nenhuma das opções", "1"],   
 
 
 ];
